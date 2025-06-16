13ca44ee4dc7f429ad2bcd8035167da84e474858
jaguarhkk <abekorir3470@gmail.com>
2025-06-16 12:24:54 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'payhero');
+
+define('API_USERNAME', 'xQ6iEAGYhrkdqidMSSas');
+define('API_PASSWORD', 'MyC4EVYGcP9aNnWOcp3YKxAqEDe6kr8Jd3X5CQVG');
