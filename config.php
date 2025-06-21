15acb4edade67452dc3296b1a5a6d14d410e8c43
jaguarhkk <abekorir3470@gmail.com>
2025-06-21 14:24:49 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,1 @@
-<?php
-// config.php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'payhero');
 
-define('API_USERNAME', 'xQ6iEAGYhrkdqidMSSas');
-define('API_PASSWORD', 'MyC4EVYGcP9aNnWOcp3YKxAqEDe6kr8Jd3X5CQVG');
